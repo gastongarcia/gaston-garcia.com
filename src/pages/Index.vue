f1b355803b2f7751d54aed29fde281366431bced
Gastón García <gaston.garcia@gmail.com>
2019-04-11 19:41:27 -0600

    Swtiching from Node/Express and Handlebars to Static site with Gridsome

---
+++ src/pages/Index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto mt-5 mb-4">
+                <h1 class="mb-4">Gastón García</h1>
+
+
+                <p>
+                    Soy un consultor, diseñador y desarrollador web. Me gusta abordar proyectos web de forma metódica. Disfruto de
+                    ayudar a empresas y personas a planear lo que realmente necesitan <i>antes</i> de empezar un sitio web. El
+                    planeamiento
+                    hace la diferencia en todo proyecto digital: reduce costos innecesarios y evita dolores de cabeza en el desarrollo.
+                </p>
+
+
+                <p>
+                    He estado involucrado en proyectos digitales para marcas como Movistar, Uber, Trident, Halls, Legrand, Mitsubishi,
+                    Mercedes-Benz
+                    y Oreo. He colaborado en los sitios web de empresas como 2CostaRicaRealEstate, Constructora Icon y La Fabbrica.
+                    También he estado involucrado en proyectos de activismo digital como &ldquo;Fuera Justo Orozco&rdquo; y &ldquo;Costa
+                    Rica Decí que Sí&rdquo;.
+                </p>
+
+
+                <p>
+                    Puedo trabajar en:
+                    <ul>
+                        <li>Analizar un proyecto digital actual</li>
+                        <li>Planificar un proyecto digital futuro</li>
+                        <li>Trabajar con su equipo en mejorar un sitio web</li>
+                        <li>Dar charlas sobre desarrollo web</li>
+                    </ul>
+                </p>
+
+                <p>
+                    Si usted tiene un sitio web que actualmente no le da resultados, o quiere iniciar un proyecto nuevo,
+                    contácteme para hacer un plan.
+                </p>
+
+                  <p><a href="mailto:">gaston@gaston-garcia.com</a></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Gastón García - Consultorías y desarrollo digital'
+  }
+}
+</script>
+
+<style>
+body {
+  border-top: 0.65em solid #cfd8dc;
+  border-right: 0.5em solid #cfd8dc;
+  border-left: 0.5em solid #cfd8dc;
+  border-bottom: 0.6em solid #cfd8dc;
+  font-family: "Roboto", sans-serif;
+  font-size: 1.1em;
+  color: #555;
+}
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: "Rubik", serif;
+  font-weight: 700;
+  color: #37474f;
+}
+
+</style>
